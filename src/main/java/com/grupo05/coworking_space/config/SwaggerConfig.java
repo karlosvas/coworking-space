8073d275e4adc8b382c951b94306284f59d0ed64
karlosvas <126987511+karlosvas@users.noreply.github.com>
2025-02-22 00:44:08 +0100

    doc: Added Swagger documentation for the happy path of controllers and DTOs

--- src/main/java/com/grupo05/coworking_space/config/SwaggerConfig.java
+++ src/main/java/com/grupo05/coworking_space/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.grupo05.coworking_space.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
@@ -9,9 +10,21 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Coworking Space API", version = "1.0", description = "Documentación de la API para gestionar usuarios, reservas y salas, para espacios de coworking", termsOfService = "http://swagger.io/terms/"), security = @SecurityRequirement(name = "bearerAuth"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, paramName = HttpHeaders.AUTHORIZATION, in = SecuritySchemeIn.HEADER, scheme = "bearer", bearerFormat = "JWT")
 public class SwaggerConfig {
+}
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Reservas")
+                        .version("1.0")
+                        .description("Documentación de la API para gestionar reservas"));
+    }
 }
--- src/main/java/com/grupo05/coworking_space/controller/ReservationController.java
+++ src/main/java/com/grupo05/coworking_space/controller/ReservationController.java
@@ -27,6 +27,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/reservations", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Reservation", description = "Enpoints para gestionar las reservas")
