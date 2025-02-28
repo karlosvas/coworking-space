d9005e5957e7b6e505425df1f8fa29b0a0e9fc7c
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2025-02-28 00:24:32 -0500

    docs: add documentation with Javadoc in the service, dto

--- src/main/java/com/grupo05/coworking_space/dto/RoomDTO.java
+++ src/main/java/com/grupo05/coworking_space/dto/RoomDTO.java
@@ -11,6 +11,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Objeto de Transferencia de Datos (DTO) para representar y transportar información de salas.
+ * 
+ * Implementa Serializable para permitir la serialización de objetos en transmisiones de red.
+ * Incluye anotaciones de Lombok para generación automática de métodos y builder.
+ *
+ * @Schema Permite configurar la documentación de Swagger para este DTO.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
@@ -18,15 +26,34 @@ import lombok.Setter;
 @Builder
 @Schema(description = "Data Transfer Object para Salas")
 public class RoomDTO implements Serializable {
-	@Schema(description = "Identificador único para sala", type = "integer", required = false, hidden = true)
+	@Schema(
+		description = "Identificador único para sala",
+		type = "integer",
+		required = false,
+		hidden = true
+	)
 	private int id;
 
-	@Schema(description = "Nombre de sala", example = "Sala de reuniones", type = "string", required = true)
+	@Schema(
+		description = "Nombre de sala",
+		example = "Sala de reuniones",
+		type = "string",
+		required = true
+	)
 	private String name;
 
-	@Schema(description = "Estado de la sala enum RoomStatus", allowableValues = { "AVAILABLE", "BUSY",
-			"MAINTENANCE", "NOT_AVAILABLE", "Available", "Busy", "Maintenance",
-			"Not Available" }, type = "string", required = true)
+	@Schema(
+		description = "Estado de la sala enum RoomStatus", allowableValues = {
+		"AVAILABLE",
+		"BUSY",
+		"MAINTENANCE",
+		"NOT_AVAILABLE",
+		"Available",
+		"Busy",
+		"Maintenance",
+		"Not Available"
+	}, type = "string", required = true
+	)
 	private RoomStatus roomStatus;
 
 	@Schema(description = "Capacidad de la sala", example = "10", type = "integer", required = true)
